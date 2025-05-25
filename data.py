cd0ae14d0c4910b92e83ba3069ee4477a292632d
Ekaterina Stepanova <Katrerrina@yandex.ru>
2025-05-25 19:38:07 +0300

    Создан файл data.py с тестовыми данными

---
+++ data.py
@@ -0,0 +1,8 @@
+class Data:
+    TEST_NAME = 'Ekaterina'
+    TEST_EMAIL = 'Katrerrina@yandex.ru'
+    TEST_PASSWORD = '1234Sun'
+
+    TEST_BUN = 'Краторная булка N-200i'
+    TEST_SAUCE = 'Соус с шипами Антарианского плоскоходца'
+    TEST_MEAT = 'Биокотлета из марсианской Магнолии'
